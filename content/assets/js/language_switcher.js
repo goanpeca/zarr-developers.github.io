c31e4d6fc83cda2d359ff866f9b59d4d81ce4d1c
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2025-05-19 09:27:16 -0500

    Simplify lang switcher

--- content/assets/js/language_switcher.js
+++ content/assets/js/language_switcher.js
@@ -1,11 +1,10 @@
 window.addEventListener("DOMContentLoaded", function() {
-  let current_language = localStorage.getItem("current_language");
   let page_language = localStorage.getItem("page_language");
-  if (current_language !== page_language) {
+
+  if (page_language !== null) {
     // No language set, defaulting to English
     current_language = page_language || "en";
   }
-
   // Set the option in the select element to have the "selected" attribute
   var selectElement = document.getElementById("select_language");
   var selectedOption = selectElement.querySelector(
@@ -24,7 +23,6 @@ function change_language() {
 
   // Get the current page URL
   const currentPageUrl = window.location.href;
-  localStorage.setItem("current_language", new_language);
   window.location.href = currentPageUrl.replace(
     "/" + page_language + "/",
     "/" + new_language + "/"
